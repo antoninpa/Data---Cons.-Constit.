a0ea25e01368254332c24b9076224b9322d46c60
antoninpa <antonin@paillet.fr>
2017-02-16 15:09:26 +0100

    Create items.py

---
+++ scraping/ConsCscrap/items.py
@@ -0,0 +1,20 @@
+from scrapy.item import Item, Field
+
+class ConsContItem(Item):
+    files = Field()
+    file_urls = Field()
+    text = Field()
+
+class listCCitem(Item):
+    dd = Field()
+    mm = Field()
+    yy = Field()
+    num_dc = Field()
+    type_dc = Field()
+    objet_dc = Field()
+    sol_dc = Field()
+    publi_dc = Field()
+
+    link_dc = Field()
+    ecli_dc = Field()
+    membres = Field()
