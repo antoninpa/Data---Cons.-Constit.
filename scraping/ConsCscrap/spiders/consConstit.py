3dee2fb608976466ccec80a51b7b0be5fbbb7ee0
antoninpa <antonin@paillet.fr>
2017-02-16 16:42:52 +0100

    Update consConstit.py

--- scraping/ConsCscrap/spiders/consConstit.py
+++ scraping/ConsCscrap/spiders/consConstit.py
@@ -3,38 +3,33 @@
 
 import re
 import csv
-#from scrapy.spiders import BaseSpider, CrawlSpider, Rule, Spider
+
 from scrapy.spiders import CrawlSpider, Rule
-
-#from scrapy.loader import ItemLoader
 from scrapy.http import Request, TextResponse, Response
-#from scrapy.selector import Selector, HtmlXPathSelector
 from scrapy.selector import Selector
-
-#from scrapy.linkextractors import LinkExtractor
 from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
 from ConsCscrap.items import listCCitem
 
 class CCspider(CrawlSpider):
     name = 'listCC'
     handle_httpstatus_list = [404, 301]
+    
+    # Page du Cons. Constit. qui renvoie, pour chaque année, à toutes les décisions prises
     start_urls = ['http://www.conseil-constitutionnel.fr/conseil-constitutionnel/francais/les-decisions/acces-par-date/decisions-depuis-1959/les-decisions-par-date.4614.html']
-    #start_urls = ['http://www.conseil-constitutionnel.fr/conseil-constitutionnel/francais/les-decisions/acces-par-date/decisions-depuis-1959/2015/sommaire-2015.142956.html']
-
     
     rules = (Rule(LxmlLinkExtractor(allow=(), restrict_xpaths="//div[@id='articlesArchives']"),
-                  callback="parse_dc", follow=True),)
+                  callback="parse_annee", follow=True),)
     
-    def parse_dc(self, response):
-        print("In Parse_DC")
-        sel = Selector(response)
-        decision = response.selector.xpath("//li[@class='ld']")
-        items = []
-        for el in decision:
+    def parse_annee(self, response):
+        
+        decisions = response.selector.xpath("//li[@class='ld']")
+
+        for el in decisions:
             item = listCCitem()
-            item['dd'] = "".join(map(unicode.strip, el.xpath("./a/text()").extract())).split()[0]
-            #item['mm'] = "".join(map(unicode.strip, el.xpath("./a/text()").extract())).split()[1]
             
+            # Pas très élégant, mais cela me permet de régler les problèmes d'encodage
+            item['dd'] = "".join(map(unicode.strip, el.xpath("./a/text()").extract())).split()[0]
+
             mois = "".join(map(unicode.strip, el.xpath("./a/text()").extract())).split()[1]
 
             if mois == 'janvier':
@@ -81,91 +76,20 @@ class CCspider(CrawlSpider):
 
             item['link_dc'] = "http://www.conseil-constitutionnel.fr"+"".join(map(unicode.strip, el.xpath("./a/@href").extract()))
 
+            # On récupère les données sur la page /année/ et on continue sur la page /décision/
+            yield Request(item['link_dc'], meta={'item':item}, callback=self.parse_decision)
 
-            #http://www.conseil-constitutionnel.fr/conseil-constitutionnel/francais/les-decisions/acces-par-date/decisions-depuis-1959/2007/2007-108-orga/decision-n-2007-108-orga-du-13-juin-2007.108012.html
-            #http://wwww.conseil-constitutionnel.fr/conseil-constitutionnel/francais/les-decisions/acces-par-date/decisions-depuis-1959/2007/2007-107-orga/decision-n-2007-107-orga-du-25-janvier-2007.108075.html'
 
-            #yield item
-            yield Request(item['link_dc'], meta={'item':item}, callback=self.dc_parse)
-            #items.append(item)
-        #return items
-            
 
-    def dc_parse(self, response):
-        sel = Selector(response)
+    def parse_decision(self, response):
         decision = response.selector.xpath("//div[@id='mainContent']")
-        items = []
         for el in decision:
             item = response.request.meta['item']
             item['ecli_dc'] = "".join(map(unicode.strip, el.xpath("./div[@id='ecli']/text()").extract()))
 
+            # Pour recenser les membres qui ont pris une décision on utilise le regex : 
             reg = "(Georges VEDEL|Louis JOXE|Robert LECOURT|Francis MOLLET-VI\xc9VILLE|L\xe9on JOZEAU-MARIGN\xc9|Daniel MAYER|Robert FABRE|Robert BADINTER|Jacques LATSCHA|Maurice FAURE|Jean CABANNES|Noelle Lenoir|Jacques ROBERT|Georges ABADIE|Marcel RUDLOFF|No\xeblle LENOIR|Roland DUMAS|Val\xe9ry GISCARD d'ESTAING|Etienne Dailly|Michel AMELLER|Alain LANCELOT|Yves GU\xc9NA|Pierre MAZEAUD|Simone VEIL|Jean-Claude COLLIARD|Monique PELLETIER|Olivier DUTHEILLET de LAMOTHE|Dominique SCHNAPPER|Pierre JOXE|Jean-Louis PEZANT|Jacqueline de GUILLENCHMIDT|Pierre STEINMETZ|Jacques BARROT|Hubert HAENEL|Jean-Louis DEBR\xc9|Guy CANIVET|Renaud DENOIX de SAINT MARC|Jacques CHIRAC|Michel CHARASSE|Claire BAZY MALAURIE|Nicolas SARKOZY|Nicole MAESTRACCI|Nicole BELLOUBET|Lionel JOSPIN|Jean-Jacques HYEST|Laurent FABIUS|Corinne LUQUIENS|Michel PINAULT)"
-
             pres_dc = u"".join(map(unicode.strip, el.xpath("./a[@id='information_seance']/following-sibling::p/text()").extract()))
             item['membres'] = re.findall(str(reg), pres_dc)
 
             yield item
-
-
-
-"""
-class DCspider(Spider):
-
-    name = 'pdfCC'
-    handle_httpstatus_list = [404, 301]
-    start_urls = ["http://www.conseil-constitutionnel.fr"]
-
-    def parse(self, response):
-        YEAR = "2014"
-        TYPE = ['dc', 'qpc']
-        mesTypes = ['DC', 'QPC']
-        with open('/home/antonin/Bureau/ConsCscrap/ConsCscrap/spiders/listCC.csv') as csvfile:
-            reader = csv. DictReader(csvfile)
-            mesURL = []
-            item = ConsContItem()
-            for row in reader:
-                try:
-                    if int(row['yy']) > 2012:
-                        if row['type_dc'] in mesTypes:
-                            #mesURL.append("http://www.conseil-constitutionnel.fr/conseil-constitutionnel/root/bank/download/"+str(row['num_dc']).replace('-','')+str(row['type_dc'])+str(row['num_dc']).replace('-', '')+str(row['type_dc']).lower()+".pdf")
-                            item['file_urls'] = ["http://www.conseil-constitutionnel.fr/conseil-constitutionnel/root/bank/download/"+str(row['num_dc']).replace('-','')+str(row['type_dc'])+str(row['num_dc']).replace('-', '')+str(row['type_dc']).lower()+".pdf"]
-                            item['text'] = str(row['dd'])+str(row['mm'])+str(row['yy'])+str(row['num_dc'])
-
-                            #item['file_urls'].append("http://www.conseil-constitutionnel.fr/conseil-constitutionnel/root/bank/download/"+str(row['num_dc']).replace('-','')+str(row['type_dc'])+str(row['num_dc']).replace('-', '')+str(row['type_dc']).lower()+".pdf")
-                            yield item
-                        else:
-                            pass
-                    else:
-                        pass
-                except ValueError:
-                    pass
-
-        yield ConsContItem(
-            file_urls=mesURL
-            )
-
-
-        for n in range(400, 442):
-            for el in TYPE:
-                item = ConsContItem()
-                items = []
-                item['file_urls'] = mesURL
-                #item['file_urls'] = ["http://www.conseil-constitutionnel.fr/conseil-constitutionnel/root/bank/download/"+YEAR+"%d" %(n)+el.upper()+YEAR+"%d" %(n)+el+".pdf"]
-                #numeroDC = "".join(map(str, re.findall('(?<=download/2014)(\d{1,4})', str(item['file_urls']))))
-                #typeDC = "".join(map(str, re.findall('(DC|QPC)', str(item['file_urls']))))
-                #item['text'] = YEAR+"-"+numeroDC+" "+typeDC
-                yield item
-
-
-
-    Quelques indications pour la recherche :
-    - 2015 : DC (710-728) ; QPC (-522) ; L(252) ; ORGA (133-
-
-
-
-     # Une manière de faire rapide pour récupérer le document depuis l'url...
-    def parse(self, response):
-        yield ConsContItem(
-            file_urls=["http://www.conseil-constitutionnel.fr/conseil-constitutionnel/root/bank/download/2005%dDC2005%ddc.pdf" %(n, n) for n in range(500, 542)]
-            )
-"""
